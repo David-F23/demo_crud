c19a5ec8088f8cc7b99d86c6caf7c8a344566166
Giovanni-Lopez <alexisange001@gmail.com>
2021-05-06 22:39:39 -0600

    Añadi el metodo actualizarEliminarRegistro con sus respectivos parametros a recibir

--- src/demo/ConexionCRUD.java
+++ src/demo/ConexionCRUD.java
@@ -8,5 +8,8 @@ public class ConexionCRUD {
     
     public void guardarRegistros(String tabla, String camposTabla, String valoresCampos){
         
-    }        
+    }  
+    public void actualizarEliminarRegistro(String tabla, String valoresCamposNuevos, String condicion){
+        
+    }
 }
